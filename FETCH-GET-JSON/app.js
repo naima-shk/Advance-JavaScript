534d4ea482dda39681c8ec937c9d0f339e5d520d
naima-shk <naimashaikh550@gmail.com>
2021-07-17 12:45:53 +0500

    'Implemented-fetch-JSON'

---
+++ FETCH-GET-JSON/app.js
@@ -0,0 +1,10 @@
+
+
+fetch('people.json').then(function (response){
+    return response.json();
+}).then (function (obj){
+    console.log(obj);
+}).catch(function (error){
+    console.log('something went wrong');
+    console.error(error);
+});
