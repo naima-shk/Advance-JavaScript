b8e082b92f866c1ee89f6474cbc366572bb66b69
naima-shk <naimashaikh550@gmail.com>
2021-07-18 13:27:08 +0500

    'implement-spread-operator'

--- Scoping/app2.js
+++ Scoping/app2.js
@@ -1,4 +1,10 @@
 window.onload =function(){
-    var city =['karachi', 'lahore', 'islamabad'];
-   console.log(...city);
+    /*var city =['karachi', 'lahore', 'islamabad'];
+   console.log(...city);*/
+
+   var nums =[3,7];
+   function addNums(a,b){
+       console.log(a+b);
+   }
+   addNums(...nums);
 }
