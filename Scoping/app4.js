6b73f5cbd1aefdb879c49919feb5d115e1f4a5b1
naima-shk <naimashaikh550@gmail.com>
2021-07-18 13:51:36 +0500

    'implement-new-string-method'

--- Scoping/app4.js
+++ Scoping/app4.js
@@ -1,5 +1,14 @@
 window.onload =function(){
     var str = 'braverrryyyy';
     //console.log(str.repeat(50));
-    console.log(str.startsWith('braverrryyyy'));
+    //console.log(str.startsWith('braverrryyyy'));
+   //console.log(str.endsWith('braverrryyyy'));
+   console.log(str.includes('c'));
 }
+
+/* New String Methods
+1- repeat
+2-startsWith
+3-endsWith
+4-includes
+*/
