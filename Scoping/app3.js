0a0b4b2ab2a5b31ffa37874f53573fced68006f2
naima-shk <naimashaikh550@gmail.com>
2021-07-18 13:36:12 +0500

    'implement-template-strings'

---
+++ Scoping/app3.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+   // var temp = `hello, this is naima`;
+    //console.log(temp);
+
+    function logNinja(name,age){
+        console.log(`my name is ${name} ans my age is ${age}`)
+    }
+    logNinja('naima', 25);
+}
