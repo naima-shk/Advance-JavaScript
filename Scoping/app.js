91f1890c5805bebbe96df09ee29138b37f854bad
naima-shk <naimashaikh550@gmail.com>
2021-07-18 13:06:47 +0500

    'implement-scoping-window-onload'

---
+++ Scoping/app.js
@@ -0,0 +1,16 @@
+//block scope
+let a= 20;
+
+if(a == 20){
+    let b= 30;
+    let c= 40;
+    //console.log(a);
+   // console.log(b);
+   // console.log(c);
+}
+//console.log(c);
+
+{
+    let d=60;
+}
+console.log(d);
---
+++ Scoping/app1.js
@@ -0,0 +1,12 @@
+window.onload =function(){
+    /*function fn(num=10){
+        console.log(num);
+    }
+    fn(20);
+*/
+
+function ninja(name,age){
+    console.log("my name is " + name + "and my age is" +age);
+}
+ninja('naima',25);
+}
