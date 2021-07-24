e4ccb3486845e5d912e35d026556fc3878fcf8e5
naima-shk <naimashaikh550@gmail.com>
2021-07-24 12:10:14 +0500

    'implement-spread-operator'

--- Spread-Operator/app.js
+++ Spread-Operator/app.js
@@ -1,3 +1,20 @@
-let city = ['karachi', 'lahore', 'quetta', 'islamabad'];
-console.log(...city);
+//practice
+
+
+//let city = ['karachi', 'lahore', 'quetta', 'islamabad'];
+//console.log(...city);
 //console.log(city);
+
+/*function sum(x, y, z,a,b) {
+    return x + y + z+a+b;
+  }
+  
+  const numbers = [1, 2, 3,4,5];
+  
+  console.log(sum(...numbers));
+  */
+ const initialNumb =[1,2,3];
+ const finalNumb =[...initialNumb, 4,5,6,7];
+ console.log(finalNumb);
+  
+  
