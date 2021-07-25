789e5cacb31cfd61188fa81c4ff362c4012d3925
naima-shk <naimashaikh550@gmail.com>
2021-07-25 12:07:54 +0500

    'implement-object-literal'

---
+++ Object-Literal-enhancement/app.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+  // variable declaration
+  var name = "Duke";
+  var color = "Brown";
+  var age = 5;
+
+  // Using Object Literal Enhancement
+  // Combines all variables into a dog object
+  var dog = {
+    name,
+    color,
+    age,
+  };
+  console.log(dog);
+};
