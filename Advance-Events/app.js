5136edf61765e47692f8fcf1fdeb878b324e8e72
naima-shk <naimashaikh550@gmail.com>
2021-08-03 14:08:54 +0500

    'implement-over-writing-event-handlers'

--- Advance-Events/app.js
+++ Advance-Events/app.js
@@ -1,5 +1,17 @@
 //events binding
-function disp(){
+function disp3(){
     alert('hello world');
 }
-document.getElementById('app').onclick=disp;
+document.getElementById('app').onclick=disp3;
+
+//over writing event handler
+function disp1(){
+    alert('hello!');
+}
+function disp2(){
+    alert('bye world');
+}
+document.getElementById('app').onclick=disp1;
+document.getElementById('app').onclick=disp2;
+
+//Dom event flow or event propagation
