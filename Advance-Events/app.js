c90b3f69346a328e47b88d0e6b1e64eeec27e6ed
naima-shk <naimashaikh550@gmail.com>
2021-08-03 14:33:30 +0500

    'implement-event-propagation'

--- Advance-Events/app.js
+++ Advance-Events/app.js
@@ -1,5 +1,5 @@
 //events binding
-function disp3(){
+/*function disp3(){
     alert('hello world');
 }
 document.getElementById('app').onclick=disp3;
@@ -13,5 +13,25 @@ function disp2(){
 }
 document.getElementById('app').onclick=disp1;
 document.getElementById('app').onclick=disp2;
-
+*/
 //Dom event flow or event propagation
+
+//bubbling phase
+/*document.getElementById('one').addEventListener('click',function(){
+console.log('1');},false);
+document.getElementById('two').addEventListener('click',function(){
+console.log('2');},false);
+document.getElementById('three').addEventListener('click',function(){
+console.log('3');},false);
+document.getElementById('four').addEventListener('click',function(){
+console.log('4');},false);*/
+
+//capture phase
+document.getElementById('one').addEventListener('click',function(){
+console.log('1');},true);
+document.getElementById('two').addEventListener('click',function(){
+console.log('2');},true);
+document.getElementById('three').addEventListener('click',function(){
+console.log('3');},true);
+document.getElementById('four').addEventListener('click',function(){
+console.log('4');},true);
